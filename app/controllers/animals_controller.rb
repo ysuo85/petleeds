603798e5fb050c90aa5b0d0677865a76b1f566e8
Alejandro Luperon <alejandroluperon@Alejandros-MacBook-Pro.local>
2015-08-08 20:19:39 -0400

    Adding update to animals_controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,15 +1,15 @@
 class AnimalsController < ApplicationController
 	def create
       	Animal.create(
-      		:user_id => params[:user][:user_id],
-      		:user_name => account.username,
-          	:user_email => account.email,
-          	:user_first_name => params[:user][:first_name],
-          	:user_last_name => params[:user][:last_name],
-          	:active_indicator => true
-      		#:primary_administrator => true,
-      		#:administrator =>  true,
-          #:primary_administrator =>true
+          :animai_id => params[:animai_id],
+          :user_id => params[:user_id],          
+          :shelter_id => params[:shelter_id],  
+          :species_id => params[:species_id],    
+          :breed_id => params[:breed_id],     
+          :behavioral_trait_id =>  params[:behavioral_trait_id], 
+          :health_trait_id => params[:health_trait_id],     
+          :vaccination_id => params[:vaccination_id],     
+          :animal_name => params[:animal_name]   
       	)
 
 
